f353c013f4227286b2d9b7f7da20638424b8b872
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-08-19 22:24:13 +0600

    done dyanimic load all of data

--- js/app.js
+++ js/app.js
@@ -1,40 +1,10 @@
-// added all utilities
-const mainSection = document.getElementById('main-section')
-// create A div
-const div = (service)=>{
-    const div = document.createElement("div");
 
-    div.innerHTML = `
-     <div class="card mt-3  mx-auto" style="max-width: 800px;">
-          <div class="row g-0">
-            <div class="col-md-4">
-              <img src=${service.imageUrl} class="img-fluid rounded-start h-100" alt="...">
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">Transport Mood ${service.vehicle}</h5>
-                <p class="card-text">${service.description}</p>
-                <p class="card-text"><small class="text-muted">Fare per kilo ${service.farePerKilo}</small> <small class="text-muted">Capacity ${service.capacity}</small></p>
-                <!-- Button trigger modal -->
-        <!--  <button type="button" class="btn btn-primary" data-bs-toggle="modal" onclick='handleBooking()' data-bs-target="#exampleModal">
-                see details
-          </button>  -->
-              </div>
-            </div>
-          </div>
-  </div>
-  `;
+  services.map(service=>{
+   div(service)
   
-    mainSection.appendChild(div);
-}
+  })
 
-const displayService = ()=>{
 
-    div(carObject)
-    div(busObject)
-    div(bikeObject)
-}
 
-displayService()
 
 
--- js/data.js
+++ js/data.js
@@ -1,4 +1,5 @@
-var carObject = {
+const services = [{
+  id: 1,
   vehicle: "Car",
   imageUrl:
     "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
@@ -7,9 +8,10 @@ var carObject = {
   capacity: 4,
   description:
     "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga eligendi veritatis expedita, dicta aut eum itaque ut tempora alias laudantium?",
-};
+},
 
-var boatObject = {
+{
+    id: 2,
     vehicle: "Car",
     imageUrl:
       "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
@@ -18,8 +20,9 @@ var boatObject = {
     capacity: 4,
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga eligendi veritatis expedita, dicta aut eum itaque ut tempora alias laudantium?",
-  };
-var bikeObject = {
+  },
+ {
+  id: 3,
   vehicle: "Bike",
   imageUrl:
     "https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmlrZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60",
@@ -30,16 +33,16 @@ var bikeObject = {
 
   description:
     "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga eligendi veritatis expedita, dicta aut eum itaque ut tempora alias laudantium?",
-};
+},
 // farePerKilo -> per kilometer cost for hiring the vehicle
 
-var busObject = {
+ {
+  id: 4,
   vehicle: "Bus",
-  imageUrl:
-    "https://images.unsplash.com/photo-1544620347-c4fd4a3d5957?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVzfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  imageUrl:"https://images.unsplash.com/photo-1544620347-c4fd4a3d5957?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVzfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
 
   farePerKilo: 3,
   capacity: 30,
   description:
     "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga eligendi veritatis expedita, dicta aut eum itaque ut tempora alias laudantium?",
-};
+}];
